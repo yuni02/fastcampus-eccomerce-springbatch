e867b8f94eaf3bef199ab0de0ca2bbd223ccd794
Yunkyeong Jang <yunkyeong@ccmedia.co.kr>
2025-11-09 11:18:30 +0900

    feat: ProductGenerator.java 코드에 무작위 행 만드는 메서드 추가

--- ecommerce-batch/src/main/java/fastcampus/ecommerce/batch/dto/ProductUploadCsvRow.java
+++ ecommerce-batch/src/main/java/fastcampus/ecommerce/batch/dto/ProductUploadCsvRow.java
@@ -13,15 +13,33 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ProductUploadCsvRow {
+
     private Long sellerId;
 
     private String category;
     private String productName;
     private String salesStartDate;
+    private String salesEndDate;
     private String productStatus;
     private String brand;
     private String manufacturer;
 
     private int salesPrice;
     private int stockQuantity;
+
+    public static ProductUploadCsvRow of(Long sellerId, String category, String productName,
+        String salesStartDate, String salesEndDate, String productStatus, String brand,
+        String manufacturer, int salesPrice, int stockQuantity) {
+        return new ProductUploadCsvRow(sellerId,
+            category,
+            productName,
+            salesStartDate,
+            salesEndDate,
+            brand,
+            productStatus,
+            manufacturer,
+            salesPrice,
+            stockQuantity);
+
+    }
 }
--- ecommerce-batch/src/main/java/fastcampus/ecommerce/batch/util/ProductGenerator.java
+++ ecommerce-batch/src/main/java/fastcampus/ecommerce/batch/util/ProductGenerator.java
@@ -1,26 +1,90 @@
 package fastcampus.ecommerce.batch.util;
 
 import fastcampus.ecommerce.batch.domain.product.Product;
+import fastcampus.ecommerce.batch.domain.product.ProductStatus;
 import fastcampus.ecommerce.batch.dto.ProductUploadCsvRow;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Random;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 
 public class ProductGenerator {
 
+    private static final Random RANDOM = new Random();
+
     public static void main(String[] args) {
         String csvFilePath = "data/random_product.csv";
-        int record = 10000000;
+        int recordCount = 10000000;
 
         try (FileWriter fileWriter = new FileWriter(
-            csvFilePath); CSVPrinter csvPrinter = new CSVPrinter(fileWriter,
+            csvFilePath); CSVPrinter printer = new CSVPrinter(fileWriter,
             CSVFormat.DEFAULT.builder().setHeader(ReflectionUtils.getFieldNames(
                 ProductUploadCsvRow.class).toArray(String[]::new)).build())) {
+
+            for (int i = 0; i < recordCount; i++) {
+                printer.printRecord(generateRecord());
+                if (i % 100000 == 0) {
+                    System.out.println("Generated " + i + " records");
+                }
+            }
 
         } catch (IOException e) {
             System.out.println("IO Exception");
         }
+    }
+
+    private static Object[] generateRecord() {
+        ProductUploadCsvRow row = randomProductRow();
+        return new Object[]{row.getSellerId(), row.getCategory(), row.getProductName(),
+            row.getSalesStartDate(), row.getSalesEndDate(),
+            row.getProductStatus(), row.getBrand(), row.getManufacturer(), row.getSalesPrice(),
+            row.getStockQuantity()};
+    }
+
+    private static ProductUploadCsvRow randomProductRow() {
+
+        String[] CATEGORIES = {"가전", "가구", " 패션", "식품", "화장품", "서적", "스포츠", "완구", "음악", "디지털"};
+        String[] PRODUCT_NAMES = {"TV", "쇼파", "셔츠", "햇반", "스킨케어 세트", "소설", "축구장", "레고", "기타",
+            "스마트폰"};
+        String[] BRANDS = {"삼성", "LG", "나이키", "아모레퍼시픽", "현대", "BMW", "롯데", "스타벅스", "도미노", "맥도날드"};
+        String[] MANUFACTURERS = {"삼성전자", "LG젅자", "나이키코리아", "아모레퍼시픽", "현대자동차", "BMW코리아", "롯데제과",
+            "스타벅스코리아", "도미노피자", "맥도날드코리아"};
+        String[] STATUSES = Arrays.stream(ProductStatus.values()).map(Enum::name)
+            .toArray(String[]::new);
+
+        return ProductUploadCsvRow.of(randomSellerId(), randomChoice(CATEGORIES),
+            randomChoice(PRODUCT_NAMES), randomDate(2020, 2023), randomDate(2020, 2023),
+            randomChoice(STATUSES), randomChoice(BRANDS), randomChoice(MANUFACTURERS),
+            randomSalesPrice(), randomStockQuantity());
+
+    }
+
+    private static int randomStockQuantity() {
+        return RANDOM.nextInt(1, 1001);
+    }
+
+    private static int randomSalesPrice() {
+        return RANDOM.nextInt(10000, 500001);
+    }
+
+    private static String randomDate(int startYear, int endYear) {
+        int year = RANDOM.nextInt(startYear, endYear + 1);
+        int month = RANDOM.nextInt(1, 12);
+        int day = RANDOM.nextInt(1, 29);
+        return LocalDate.of(year, month, day).toString();
+
+    }
+
+    private static String randomChoice(String[] array) {
+        return array[RANDOM.nextInt(array.length)];
+    }
+
+    private static Long randomSellerId() {
+
+        return RANDOM.nextLong(1, 101);
     }
 
 }
